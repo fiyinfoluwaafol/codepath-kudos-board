0592dfe2f398577784d031367c9d4528282b2623
Fiyinfoluwa Afolayan <fiyinfoluwaafol@meta.com>
2024-06-18 17:06:55 -0700

    rendering board cards from database

--- frontend/src/assets/components/BoardCard/BoardCard.jsx
+++ frontend/src/assets/components/BoardCard/BoardCard.jsx
@@ -1,11 +1,11 @@
 import "./BoardCard.css"
 
-function BoardCard () {
+function BoardCard ({title, category}) {
     return (
        <div className="board-card">
             <img src='https://placehold.co/250x400' alt='' className="board-cover"/>
-            <h2>Board Title</h2>
-            <p>Board Category</p>
+            <h2>{title}</h2>
+            <p>{category}</p>
             <div className='card-bttn'>
                 <button>View Board</button>
                 <button>Delete Board</button>
--- frontend/src/assets/components/BoardList/BoardList.jsx
+++ frontend/src/assets/components/BoardList/BoardList.jsx
@@ -1,13 +1,22 @@
 import './BoardList.css';
 import BoardCard from '../BoardCard/BoardCard';
-function BoardList() {
+function BoardList({boards}) {
+
+  const boardLst = boards.map((board) => {
+    return (
+      <BoardCard
+        key={board.id}
+        title={board.title}
+        category={board.category}
+      />
+    );
+  });
+
   return (
     <div className='board-container'>
         <button className="board-list-button">Create a New Board</button>
         <div className="board-list">
-            <BoardCard />
-            <BoardCard />
-            <BoardCard />
+            {boardLst}
         </div>
     </div>
     )
