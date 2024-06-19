a15ce08574b3755337a8303c1c2bf6ca3d644d58
Fiyinfoluwa Afolayan <fiyinfoluwaafol@meta.com>
2024-06-19 09:59:31 -0700

    delete board functionality added

--- frontend/src/components/BoardCard/BoardCard.jsx
+++ frontend/src/components/BoardCard/BoardCard.jsx
@@ -1,6 +1,6 @@
 import "./BoardCard.css"
 
-function BoardCard ({title, category}) {
+function BoardCard ({key, title, category, onBoardDelete}) {
     return (
        <div className="board-card">
             <img src='https://placehold.co/250x400' alt='' className="board-cover"/>
@@ -8,7 +8,7 @@ function BoardCard ({title, category}) {
             <p>{category}</p>
             <div className='card-bttn'>
                 <button>View Board</button>
-                <button>Delete Board</button>
+                <button onClick={onBoardDelete}>Delete Board</button>
             </div>
        </div>
     )
